695548575ff26c8fda940901a3b44f138271a932
Ildus K <k-dus@yandex.ru>
2014-01-06 01:55:37 +0700

    initial

---
+++ install.sh
@@ -0,0 +1,16 @@
+sudo apt-get install libpcre3-dev libssl-dev
+sudo apt-get install libgd2-xpm-dev
+sudo apt-get install zlibc zlib1g zlib1g-dev
+sudo adduser --system --no-create-home --disabled-login --disabled-password --group nginx
+
+cd nginx-1.5.8
+./configure --prefix=/opt/nginx --user=nginx \
+    --with-http_image_filter_module \
+    --add-module=../redis-nginx-module-0.3.7 \
+    --with-http_ssl_module \
+    --add-module=../ngx_devel_kit-0.2.19 \
+    --add-module=../set-misc-nginx-module-0.23
+
+make
+sudo make install
+sudo mkdir /opt/nginx/conf/sites-enabled
